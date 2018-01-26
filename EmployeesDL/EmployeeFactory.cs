9fea612c55a87a2430cbf0cbd2d6367db522e9e4
Mayur Agarwal <mayurinnovate@live.com>
2018-01-27 00:29:21 +0530

    GetEmployees API

---
+++ EmployeesDL/EmployeeFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeesDL
+{
+    public class EmployeeFactory
+    {
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using (var entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
---
+++ EmployeesService/Controllers/EmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesDL;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace EmployeesService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            var employeeFactory = new EmployeeFactory();
+                return employeeFactory.GetEmployees();
+            
+        }
+    }
+}
